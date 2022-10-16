4433428fa73c2d0d8c8c2c0e10380261b3499138
mmslotin <balu54@yandex.ru>
2022-10-16 13:54:10 +0300

    New test added

--- src/test/java/SelenideTest.java
+++ src/test/java/SelenideTest.java
@@ -2,6 +2,8 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class SelenideTest {
@@ -14,10 +16,10 @@ public class SelenideTest {
 
     @Test
     void shouldFindSelenideRepositoryAtFirstPlace(){
-        open("https://github.com/");
-        $("[data-test-selector=nav-search-input]").setValue("selenide").pressEnter();
-        $$(".repo-list li").first().$("a").click();
-        $("#repository-container-header").shouldHave(text("selenide / selenide"));
-
+            open("https://github.com/selenide/selenide");
+            $("#wiki-tab").click();
+            $("#wiki-pages-filter").setValue("SoftAssertions").pressEnter();
+            $$(".details-reset").findBy(visible).shouldBe(text("SoftAssertions")).click();
+            $(".Layout-main").$(byText("3. Using JUnit5 extend test class:")).shouldBe(text("3. Using JUnit5 extend test class:"));
+        }
     }
-}
